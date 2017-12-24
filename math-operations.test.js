338b269fa453eb457e598ca5fad0ce03fec86014
brarian <b.namonywa@gmail.com>
2017-12-23 20:07:11 -0600

    Extracts sum function from test file.

--- math-operations.test.js
+++ math-operations.test.js
@@ -1,8 +1,5 @@
 'use strict';
-
-function sum(a, b){
-  return a+b;
-}
+const {sum} = require('./sum');
 
 test('2 plus 2 should return 4', function(){
   expect(sum(2, 2)).toBe(4);
@@ -11,3 +8,4 @@ test('2 plus 2 should return 4', function(){
 test('3 plus 5 should return 8', function(){
   expect(sum(3, 5)).toBe(8);
 });
+
