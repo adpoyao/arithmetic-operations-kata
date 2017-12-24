06d6ed19b73dcb77cf0e6a1413351d56cf1a0416
Nicci Williams <nicci@nicciwill.com>
2017-12-23 20:50:46 -0500

    Tests the addition of 2 different numbers.

--- math-operations.test.js
+++ math-operations.test.js
@@ -7,3 +7,7 @@ function sum(a, b){
 test('2 plus 2 should return 4', function(){
   expect(sum(2, 2)).toBe(4);
 });
+
+test('3 plus 5 should return 8', function(){
+  expect(sum(3, 5)).toBe(8);
+});
